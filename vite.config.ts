4181e153040abb5716c46f334daec4fcaba66360
Mohanraj <mohanraj.cyber@gmail.com>
2026-03-08 18:52:26 +0530

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' && process.env.GITHUB_PAGES ? '/QuantamAI/' : '/',
+  base: '/QuantamAI/',
   plugins: [
     react({
       jsxRuntime: 'automatic'
